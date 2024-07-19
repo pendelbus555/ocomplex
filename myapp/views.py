9706f6c563e0ed69b4a64403fef9033bb5c9210c
mrusipusi <mrusipusi@mail.ru>
2024-07-19 11:58:10 +0300

    Add last searched city

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,10 @@ from .services import get_weather, get_city_coordinates
 
 
 def index(request):
+    last_city = request.COOKIES.get('last_city', None)
+    print(last_city)
+    weather_data = None
+
     if request.method == "POST":
         form = CityForm(request.POST)
         if form.is_valid():
@@ -11,11 +15,12 @@ def index(request):
             lat, lng = get_city_coordinates(city_name)
             if lat and lng:
                 weather_data = get_weather(lat, lng)
-                print(weather_data)
-                return render(request, "myapp/index.html", {"form": form, "weather_data": weather_data})
+                response = render(request, "myapp/index.html", {"form": form, "weather_data": weather_data})
+                response.set_cookie('last_city', city_name, max_age=30*24*60*60)  # 30 дней
+                return response
             else:
                 form.add_error('city_name', 'Invalid city name. Please enter a valid city or try again later.')
     else:
         form = CityForm()
 
-    return render(request, "myapp/index.html", {"form": form})
+    return render(request, "myapp/index.html", {"form": form, 'last_city': last_city})
